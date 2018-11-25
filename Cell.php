bd434da0926dfa7155a9423c43b7a6fe71bf6460
Mone20 <44345543+Mone20@users.noreply.github.com>
2018-11-25 21:26:14 +0400

    Create Cell.php

---
+++ Cell.php
@@ -0,0 +1,13 @@
+<?php
+class Cell
+{
+	public  $cellNumber;
+	public $cellCondition;
+	function __construct($num,$condition)
+	{
+$this->cellNumber=$num;
+$this->cellCondition=$condition;
+	}
+
+}
+?>
