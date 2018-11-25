58ac3f49e3070a8d00d6d1dc17645c8361fad813
Mone20 <44345543+Mone20@users.noreply.github.com>
2018-11-25 21:26:48 +0400

    Update Player.php

--- Player.php
+++ Player.php
@@ -1,1 +1,18 @@
 
+<?php
+class Player
+{
+	public  $fieldArray;
+	public  $fieldMoveArray;
+	public  $name;
+	public  $isMove;
+	function __construct($fieldArray,$name,$fieldMoveArray,$ismove)
+	{
+$this->fieldArray=$fieldArray;
+$this->fieldMoveArray=$fieldMoveArray;
+$this->name=$name;
+$this->isMove=$ismove;
+	}
+
+}
+?>
