943cf02404de2ba4dad29603fbb6bd886cd45ced
Mone20 <44345543+Mone20@users.noreply.github.com>
2018-12-12 00:37:26 +0400

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,131 +1,263 @@
 <?php
-require 'Cell.php';
-require 'Player.php';
 class Controller
 {
+	function getMoveCells()
+	{
+		$pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+		$playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MAX(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        
+        if($playerMassive['is_attack'])
+        { 
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MIN(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        }
+         $playerId=$playerMassive['players_id'];
+            $fieldPdo=$playerPdo->query("SELECT * FROM fields WHERE players_id=$playerId");
+            $fieldMassive=$playerPdo->fetch();
+            $fieldId=$fieldMassive['fields_id'];
+            $cellsPdo=$pdo->query("SELECT * FROM cells WHERE id=$fieldId");
+            return $cellsPdo;
+ 
+	}
+
+
+    function getMovePlayer()
+    {
+        $pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+        $cells=[];
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MAX(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        
+        if(!$playerMassive['is_attack'])
+        {
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MIN(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+
+        }
+            $playerId=$playerMassive['players_id'];
+            $fieldPdo=$playerPdo->query("SELECT * FROM fields WHERE players_id=$playerId");
+            $fieldMassive=$playerPdo->fetch();
+            $fieldId=$fieldMassive['fields_id'];
+            $cellsPdo=$pdo->query("SELECT * FROM cells WHERE id=$fieldId");
+            while ($tableCells=$cellsPdo->fetch()) 
+            {
+                $cells[]=new Cell($tableCells['num_cell'],$tableCells['cell_condition']);
+            }
+            $player=new Player($playerMassive['name_player'],$cells,$arrPlayer['is_attack']);
+            
+             return $player;
+    
+    }
+     function getNotMovePlayer()
+    {
+           $pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+        $cells=[];
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MAX(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        
+        if($playerMassive['is_attack'])
+        {
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MIN(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+
+        }
+            $playerId=$playerMassive['id'];
+            $fieldPdo=$pdo->query("SELECT * FROM fields WHERE player_id=$playerId");
+            $fieldMassive=$fieldPdo->fetch();
+            $fieldId=$fieldMassive['id'];
+            $cellsPdo=$pdo->query("SELECT * FROM cells WHERE id=$fieldId");
+            
+            while ($tableCells=$cellsPdo->fetch()) 
+            {
+                $cells[]=new Cell($tableCells['num_cell'],$tableCells['cell_condition']);
+            }
+            
+            $player=new Player($playerMassive['name_player'],$cells,$arrPlayer['is_attack']);
+            
+            return $player;
+    
+    }
     function changeOfCourse()
 {
-    $fieldResource1="C:\\battleship\\field1.json";
-    $player=file_get_contents($fieldResource1);
-    $player=json_decode($player);
-    $player->isMove=!($player->isMove);
-    $player=json_encode($player);
-    $resource1=fopen($fieldResource1,"w");
-    fputs($resource1,$player);
-    $fieldResource2="C:\\battleship\\field2.json";
-    $player=file_get_contents($fieldResource2);
-    $player=json_decode($player);
-    $player->isMove=!($player->isMove);
-    $player=json_encode($player);
-    $resource2=fopen($fieldResource2,"w");
-    fputs($resource2,$player);
+	
+	    $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MAX(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        $numplayer=$playerMassive['num_player'];
+        if($playerMassive['is_attack'])
+        {
+        	$pdo->exec("UPDATE players SET is_attack=false WHERE num_player=$numPlayer");
+        }
+        else
+        {
+        	$pdo->exec("UPDATE players SET is_attack=true WHERE num_player=$numPlayer");
+        }
+        $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MIN(id) FROM players)");
+        $playerMassive=$playerPdo->fetch();
+        $numplayer=$playerMassive['num_player'];
+        if($playerMassive['is_attack'])
+        {
+        	$pdo->exec("UPDATE players SET is_attack=false WHERE num_player=$numPlayer");
+        }
+        else
+        {
+        	$pdo->exec("UPDATE players SET is_attack=true WHERE num_player=$numPlayer");
+        }
+//     $fieldResource1="C:\\battleship\\field1.json";
+//     $player=file_get_contents($fieldResource1);
+//     $player=json_decode($player);
+//     $player->isMove=!($player->isMove);
+//     $player=json_encode($player);
+//     $resource1=fopen($fieldResource1,"w");
+//     fputs($resource1,$player);
+//     $fieldResource2="C:\\battleship\\field2.json";
+//     $player=file_get_contents($fieldResource2);
+//     $player=json_decode($player);
+//     $player->isMove=!($player->isMove);
+//     $player=json_encode($player);
+//     $resource2=fopen($fieldResource2,"w");
+//     fputs($resource2,$player);
 }
 
 function setPlayer()
 {
-    $cellMassive=[];
-    $moveCellMassive=[];
+$pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+$cellMassive=[];
+$moveCellMassive=[];
     for($i=1;$i<=100;$i++)
     {
-    	$cellMassive[]=new Cell($i,0);
-    	$moveCellMassive[]=new Cell($i,0);
+     $cellMassive[]=new Cell($i,0);
+     $moveCellMassive[]=new Cell($i,0);
     }
     foreach ($_GET['cell'] as $value) 
     {
         
-    	$cellMassive[(int)$value-1]->cellCondition=1;
+     $cellMassive[(int)$value-1]->cellCondition=1;
     }
-    if(filesize("C:\\battleship\\field1.json")!=0)
-    {
-    	$fieldResource=fopen("C:\\battleship\\field2.json", "w");
-    	$player=new Player($cellMassive,$_GET['name'],$moveCellMassive,false);
-    }
-    else
-    {
-    $player=new Player($cellMassive,$_GET['name'],$moveCellMassive,true);
-    $fieldResource=fopen("C:\\battleship\\field1.json", "w");
-    }
-    $player=json_encode($player);
-    $isOpen=fputs($fieldResource,$player);
+   
+    $num=$pdo->query("SELECT COUNT(*) as count FROM players")->fetchColumn();//проверка на пустоту таблицы players
+
+        if($num==0)
+        {
+             $player=new Player($cellMassive,$_GET['name'],$moveCellMassive,true);
+                $timeBegin=date('l jS \of F Y h:i:s A');
+                $pdo->exec("INSERT INTO games VALUES(DEFAULT,'$timeBegin',' ',' ',' ')");
+                $pdo->exec("INSERT INTO players VALUES(DEFAULT,1,'$player->name',true)");
+        }
+        else{
+        $player=new Player($cellMassive,$_GET['name'],$moveCellMassive,false);
+        $pdo->exec("INSERT INTO players VALUES(DEFAULT,2,'$player->name',false)");
+        }
+        if($num!=0){
+            $playerPdo=$pdo->query("SELECT * FROM players WHERE id=(SELECT MAX(id) FROM players)");
+            $playerArray=$playerPdo->fetch();
+            $g=$pdo->query("SELECT * FROM games");
+            $arrGame=$g->fetch();
+            $idGame=$arrGame['id'];
+            $idPlayer=$arrPlayer['id'];
+            $count=$pdo->exec("INSERT INTO fields VALUES(DEFAULT,$idPlayer,$idGame);");
+            $f=$pdo->query("SELECT * FROM fields WHERE id=(SELECT MAX(id) FROM fields)");
+            $arrField=$f->fetch();
+            $idField=$arrField['id'];
+
+            foreach ($cells as $cell) {
+                $count=$pdo->exec("INSERT INTO cells VALUES(DEFAULT,$cell->numCell,$cell->cellCondition,$idField);");
+            }
+        }
+    
+//     if(filesize("C:\\battleship\\field1.json")!=0)
+//     {
+//       $fieldResource=fopen("C:\\battleship\\field2.json", "w");
+//       $player=new Player($cellMassive,$_GET['name'],$moveCellMassive,false);
+//     }
+//     else
+//     {
+//     $player=new Player($cellMassive,$_GET['name'],$moveCellMassive,true);
+//     $fieldResource=fopen("C:\\battleship\\field1.json", "w");
+//     }
+//     $player=json_encode($player);
+//     $isOpen=fputs($fieldResource,$player);
 
 }
 
 function makeAStep()
 {
-
- $player=file_get_contents($this->getMoveResource());
- $player=json_decode($player);
- $player2=file_get_contents($this->getNotMoveResource());
- $player2=json_decode($player2);
-$Hit=0;
+$pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+//заполнение с базы ДОПИСАТЬ!!!
+$player2=getNotMovePlayer();
     echo 'Ход игрока:'.$player->name;
     if(!empty($_GET['numOfCell']))
      {   
+     	$numberOfCell=(int)$_GET['numOfCell'];
 
-    if($player2->fieldArray[(int)$_GET['numOfCell']]->cellCondition==1)
-	{
-		$player2->fieldArray[(int)$_GET['numOfCell']]->cellCondition=3;//состояние при пробитой палубе=3
-		$player->fieldMoveArray[(int)$_GET['numOfCell']]=2;
-        $Hit=1;
-		
-	}
-	if(	$player2->fieldArray[(int)$_GET['numOfCell']]->cellCondition!=1)
-	{
+    if($player2->fieldArray[$numberOfCell]->cellCondition==1)
+ {
+ 	  $cellsPdo=getMoveCells();
+ 	  $pdo->exec("UPDATE cells SET cell_condition=3 WHERE num_cell=$numberOfCell");
+ 	  $player2->fieldArray->cellCondition[$numberOfCell]=3;
+      //состояние при пробитой палубе=3
+     
+        
+ }
+ if( $player2->fieldArray[$numberOfCell]->cellCondition!=1)
+ {
+	$pdo->exec("UPDATE cells SET cell_condition=2 WHERE num_cell=$numberOfCell");
+        //состояние при промахе=2
+         $player2->fieldArray->cellCondition[$numberOfCell]=2;
+         $this->changeOfCourse();
+ }
+ $player2=getNotMovePlayer();
+ $hit=0;
+foreach ($player2->fieldArray as $value) {
+	if($value->cellCondition==1)
+		$hit=$hit+1;
+}
+if($hit==0)
+{
+	    $gamePdo=$pdo->query("SELECT * FROM games WHERE id=(SELECT MAX(id) FROM games)");
+        $gameMassive=$gamePdo->fetch();
+        $timeBegin=$gameMassive['game_start_time'];
+        $winner=getMovePlayer();
+        $nameWinner=$winner->name;
+        $nameLoser=$player2->name;
+        $timeEnd=date('l jS \of F Y h:i:s A');
+	    $game=new Game($timeBegin,$timeEnd,$nameWinner,$nameLoser);
+	    $pdo->exec("UPDATE games SET game_end_time=$timeEnd WHERE id=(SELECT MAX(id) FROM games)");
+	    $pdo->exec("UPDATE games SET winner_name=$nameWinner WHERE id=(SELECT MAX(id) FROM games)");
+	    $pdo->exec("UPDATE games SET loser_name=$nameLoser WHERE id=(SELECT MAX(id) FROM games)");
+	    echo 'Победил:'.$nameWinner;
+}
 
-			$player2->fieldArray[(int)$_GET['numOfCell']]->cellCondition=2;//состояние при промахе=2
-			$player->fieldMoveArray[(int)$_GET['numOfCell']]=1;
-            echo $_GET['numOfCell'];	
-	}
-	$player=json_encode($player);
-    $isOpen=(boolean)fputs(fopen($this->getMoveResource(),"w"),$player);
-    $player2=json_encode($player2);
-    $isOpen=(boolean)fputs(fopen($this->getNotMoveResource(),"w"),$player2);
-    if($Hit==0)
-    $this->changeOfCourse();
-    
+
+ 
+
 }
 
 }
  
-function setController()
+function setController() 
 {
-	switch($_GET['action'])
+    $this->createHtml();
+
+   $pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
+   if(!empty($_GET['action'])){ 
+ switch($_GET['action'])
     {
-    	case 'setPlayer':
-    	$this->setPlayer();
-    	break;
-    	case 'makeAStep':
-    	$this->makeAStep();
-    	break;
-    	
+     case 'setPlayer':
+     $this->setPlayer();
+     break;
+     case 'makeAStep':
+     $this->makeAStep();
+     break;
+        
     }
+}
 }
 
-function getMoveResource()
-{
-	$fieldResource="C:\\battleship\\field1.json";
-    $player=file_get_contents($fieldResource);
-    $player=json_decode($player);
-    if($player->isMove==false)
-    {
-    return "C:\\battleship\\field2.json";
-    }
-    return $fieldResource;
-}
-function getNotMoveResource()
-{
-    $fieldResource="C:\\battleship\\field1.json";
-    $player=file_get_contents($fieldResource);
-    $player=json_decode($player);
-    if($player->isMove==true)
-    {
-    return "C:\\battleship\\field2.json";
-    }
-    return $fieldResource;
-}
 
 function createHtml()
 {
+$pdo=new PDO('pgsql:host=localhost; dbname=postgres', 'rodion','qwerty1234');
 $head='<!DOCTYPE html>
 <html>
 <head>
@@ -284,7 +416,10 @@ $body='<body>
 </table>';
 $firstsubmitForm='<input type="submit" name="action" value="setPlayer" form="my_form">';
 $secondsubmitForm='<input type="submit" name="action" value="makeAStep" form="my_form">';
-if(filesize("C:\\battleship\\field2.json")!=0)
+$num=$pdo->query("SELECT COUNT(*) as count FROM players")->fetchColumn();
+
+
+if($num==2)
 {
  $body='
 <body>
@@ -304,57 +439,59 @@ if(filesize("C:\\battleship\\field2.json")!=0)
 
     </style>
 <table>';
- $player=file_get_contents($this->getMoveResource());
- $player=json_decode($player);
- $index=0;
+$notMovePlayer=$this->getNotMovePlayer();
+$index=0;
  for($i=0;$i<10;$i++) {
- 	$body=$body.'
- 	<tr>';
- 	for($j=0;$j<10;$j++)
- 	{
- 		$index=$index+1;
- 		if($player->fieldMoveArray[$i+$j]->cellCondition==0)
- 		{
+    $body=$body.'
+    <tr>';
+    for($j=0;$j<10;$j++)
+    {
+        $index=$index+1;
+        if($notMovePlayer->fieldArray[$i+$j]->cellCondition==0)
+        {
         $body=$body.'
         <td bgcolor="#999900"><a href="index.php?action=makeAStep&numOfCell='.$index.'"></a></td>';
- 		}
- 		if($player->fieldMoveArray[$i+$j]->cellCondition==1)
- 		{
+        }
+        if($notMovePlayer->fieldArray[$i+$j]->cellCondition==1)
+        {
             
- 			$body=$body.'
- 			<td bgcolor="#FFFFFF"><a action=makeAStep></a></td>';
- 		}
- 		if($player->fieldMoveArray[$i+$j]->cellCondition==2)
- 		{
+            $body=$body.'
+            <td bgcolor="#FFFFFF"><a action=makeAStep></a></td>';
+        }
+        if($notMovePlayer->fieldArray[$i+$j]->cellCondition==2)
+        {
             
- 			$body=$body.'
- 			<td bgcolor="#FF0000"><a action=makeAStep></a></td>';
- 		}
- 	}
- 	$body=$body.'
- 	</tr>';
- 	
+            $body=$body.'
+            <td bgcolor="#FF0000"><a action=makeAStep></a></td>';
+        }
+         if($notMovePlayer->fieldArray[$i+$j]->cellCondition==3)
+        {
+            
+            $body=$body.'
+            <td bgcolor="#FFFF00"><a action=makeAStep></a></td>';
+        }
+    }
+    $body=$body.'
+    </tr>';
+    
  }
 
  echo $head.$body.'
 </table>
 </body>
 </html>';
+}
 
- }
-else
-{
-if(filesize("C:\\battleship\\field1.json")!=0)
+if($num==1)
 {
 echo $head.$body.$secondsubmitForm.'</body>
 </html>';
-
 }
 else
 echo $head.$body.$firstsubmitForm.'</body>
 </html>';
 }
 
-}
-}
+} 
+
 ?>
